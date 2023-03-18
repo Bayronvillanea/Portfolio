67933938eaad6d725a436045a120137829ddef4d
Bayron Villanea Hidalgo <bayronvillaneahidalgo.30@gmail.com>
2023-03-18 09:58:09 -0600

    Scroll

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,34 +1,71 @@
 import React from 'react'
+import { Link, animateScroll as scroll } from 'react-scroll';
 import '../Estilos/Header.css'
 
 export default function Header() {
+    const scrollTop = ()=>{
+        scroll.scrollToTop()
+    }
   
   return (
     <div className='Contenedor-header'>
-        <header>
-            
-       
-          <nav className='Contenedor-nav-header'>
-              <ul className='ul-heaeder'>
-                  <li className='li-header'>
-                      <a className='Header-a' href="/Home">Home</a>
-                  </li>
-                  <li>
-                      <a className='Header-a' href="#aboutP">About Me</a>
-                  </li>
-                  <li>
-                      <a className='Header-a' href="#SkillsP">Skills</a>
-                  </li>
-                  <li>
-                      <a className='Header-a' href="#PortfolioP">Porfolio</a>
-                  </li>
-                  <li>
-                      <a className='Header-a' href="#ContactP">Contact Me</a>
-                  </li>
-              </ul>
-            
-          </nav>
-        </header>  
-    </div>
+    <header>
+      <nav className='Contenedor-nav-header'>
+        <ul className='ul-heaeder'>
+          <li className='li-header'>
+            <Link
+              className='Header-a'
+              to='Home'
+              smooth={true}
+              duration={500}
+              onClick={scrollTop}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='aboutP'
+              smooth={true}
+              duration={500}
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='SkillsP'
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='PortfolioP'
+              smooth={true}
+              duration={500}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='ContactP'
+              smooth={true}
+              duration={800}
+            >
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
   )
 }
