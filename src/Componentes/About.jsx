ccba31958f7848d9960432ae5c301afe05d12090
Bayron Villanea Hidalgo <bayronvillaneahidalgo.30@gmail.com>
2023-03-18 10:18:48 -0600

    text

--- src/Componentes/About.jsx
+++ src/Componentes/About.jsx
@@ -5,11 +5,10 @@ export default function About() {
   return (
     <div className='about'>
         <h2 className='about-h2' id='aboutP'>About Me</h2>
-        <p className='about-p'>I'm Bayron Villanea currently student of Software engineering. <br/>
-I am passionate about web development and that's why every day I try to develop my skills as front.<br/>
-I am an autodidactic person and most of the knowledge I have about It is about a document<br/> that I read I learned it by myself through different courses and platforms.<br/>
-
-Learning about technology by my own has taught me that all is about setting goals and <br/>try to achievement even if I have to start hundred of times.</p>
+        <p className='about-p'>currently student of Software engineering.
+I am passionate about web development<br/> and that's why every day I try to develop my skills as front-end.<br/>
+I am an autodidactic person and most of the knowledge<br/> I have learned it by reading  documents , different courses and platforms.<br/>
+Learning about technology by my own has taught me that all is about<br/> setting goals and try to achieve even if I have to start hundreds of times.</p>
     </div>
   )
 }
