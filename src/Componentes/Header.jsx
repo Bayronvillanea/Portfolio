25c9744df3a2cea4d4f20ff437faa86eb667c0b6
Bayronvillanea <bayronvillaneahidalgo.30@gmail.com>
2023-05-28 19:35:57 -0600

    Camnbios de munu Hamburgesa

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,72 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
-import '../Estilos/Header.css'
+import '../Estilos/Header.css';
 
 export default function Header() {
-    const scrollTop = ()=>{
-        scroll.scrollToTop()
-    }
-  
+  const [showNav, setShowNav] = useState(false);
+
+  const scrollTop = () => {
+    scroll.scrollToTop();
+  };
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <div className='Contenedor-header'>
-    <header>
-      <nav className='Contenedor-nav-header'>
-        
-        <ul className='ul-heaeder'>
-          <li className='li-header'>
-            <Link
-              className='Header-a'
-              to='Home'
-              smooth={true}
-              duration={500}
-              onClick={scrollTop}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='Header-a'
-              to='aboutP'
-              smooth={true}
-              duration={500}
-            >
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='Header-a'
-              to='SkillsP'
-              smooth={true}
-              duration={500}
-            >
-              Skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='Header-a'
-              to='PortfolioP'
-              smooth={true}
-              duration={500}
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='Header-a'
-              to='ContactP'
-              smooth={true}
-              duration={800}
-            >
-              Contact Me
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </div>
-  )
+      <header>
+        <nav className={`Contenedor-nav-header ${showNav ? 'show-nav' : ''}`}>
+          <div className='hamburger' onClick={toggleNav}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul className={`ul-heaeder ${showNav ? 'show-nav' : ''}`}>
+            <li className='li-header'>
+              <Link
+                className='Header-a'
+                to='Home'
+                smooth={true}
+                duration={500}
+                onClick={scrollTop}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='Header-a'
+                to='aboutP'
+                smooth={true}
+                duration={500}
+              >
+                About Me
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='Header-a'
+                to='SkillsP'
+                smooth={true}
+                duration={500}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='Header-a'
+                to='PortfolioP'
+                smooth={true}
+                duration={500}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='Header-a'
+                to='ContactP'
+                smooth={true}
+                duration={800}
+              >
+                Contact Me
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
