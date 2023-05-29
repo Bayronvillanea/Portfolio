29e42516ba26e1180e3d6f34e8a82755fb096218
Bayronvillanea <bayronvillaneahidalgo.30@gmail.com>
2023-05-28 19:36:17 -0600

    cambio nav

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -14,69 +14,67 @@ export default function Header() {
   };
 
   return (
-    <div className='Contenedor-header'>
-      <header>
-        <nav className={`Contenedor-nav-header ${showNav ? 'show-nav' : ''}`}>
-          <div className='hamburger' onClick={toggleNav}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <ul className={`ul-heaeder ${showNav ? 'show-nav' : ''}`}>
-            <li className='li-header'>
-              <Link
-                className='Header-a'
-                to='Home'
-                smooth={true}
-                duration={500}
-                onClick={scrollTop}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className='Header-a'
-                to='aboutP'
-                smooth={true}
-                duration={500}
-              >
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                className='Header-a'
-                to='SkillsP'
-                smooth={true}
-                duration={500}
-              >
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link
-                className='Header-a'
-                to='PortfolioP'
-                smooth={true}
-                duration={500}
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                className='Header-a'
-                to='ContactP'
-                smooth={true}
-                duration={800}
-              >
-                Contact Me
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header className='Contenedor-header'>
+      <nav className={`Contenedor-nav-header ${showNav ? 'show-nav' : ''}`}>
+        <div className='hamburger' onClick={toggleNav}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={`ul-header ${showNav ? 'show-nav' : ''}`}>
+          <li className='li-header'>
+            <Link
+              className='Header-a'
+              to='Home'
+              smooth={true}
+              duration={500}
+              onClick={scrollTop}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='aboutP'
+              smooth={true}
+              duration={500}
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='SkillsP'
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='PortfolioP'
+              smooth={true}
+              duration={500}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='Header-a'
+              to='ContactP'
+              smooth={true}
+              duration={800}
+            >
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
