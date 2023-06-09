da050fc413421e2beca679bb0e95e563111d5db5
Bayronvillanea <bayronvillaneahidalgo.30@gmail.com>
2023-06-09 11:45:03 -0600

    CambioTextParrafo

--- src/Componentes/About.jsx
+++ src/Componentes/About.jsx
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <div className='about'>
         <h2 className='about-h2' id='aboutP'>About Me</h2>
-        <p className='about-p'>currently student of Software engineering.
-I am passionate about web development<br/> and that's why every day I try to develop my skills as front-end.<br/>
-I am an autodidactic person and most of the knowledge<br/> I have learned it by reading  documents , different courses and platforms.<br/>
-Learning about technology by my own has taught me that all is about<br/> setting goals and try to achieve even if I have to start hundreds of times.</p>
+        <p className='about-p'>Currently student of Software engineering.<br/>
+I am passionate about web development and that's why every day I try to develop my skills as front-end.<br/>
+I am an autodidactic person and most of the knowledge I have learned it by reading  documents , different courses and platforms.<br/>
+Learning about technology by my own has taught me that all is about setting goals and try to achieve even if I have to start hundreds of times.</p>
     </div>
   )
 }
