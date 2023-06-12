010473fd7f0629a1bf3766be3579657fae3804df
Bayronvillanea <bayronvillaneahidalgo.30@gmail.com>
2023-06-12 09:22:14 -0600

    cambio about

--- src/Componentes/About.jsx
+++ src/Componentes/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <h2 className='about-h2' id='aboutP'>About Me</h2>
         <p className='about-p'>Currently student of Software engineering.<br/>
 I am passionate about web development and that's why every day I try to develop my skills as front-end.<br/>
-I am an autodidactic person and most of the knowledge I have learned it by reading  documents , different courses and platforms.<br/>
+I am an autodidactic person and most of the knowledge I have,  learned it by reading  documents , different courses and platforms.<br/>
 Learning about technology by my own has taught me that all is about setting goals and try to achieve even if I have to start hundreds of times.</p>
     </div>
   )
