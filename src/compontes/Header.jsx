841623ef78b9763fc430e592b8ad219d7d9070c1
Bayron Villanea <bayronvillaneahidalgo.30@gmail.com>
2023-11-02 21:04:16 -0600

    tamaño x

--- src/compontes/Header.jsx
+++ src/compontes/Header.jsx
@@ -51,7 +51,7 @@ export default function Header() {
         <button onClick={toggleMenu} className="text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-6 w-6 mr-4 mt-2"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
