8eb86138cc479aceb6d3c420e108e00077e356aa
Bayron Villanea <bayronvillaneahidalgo.30@gmail.com>
2023-11-04 15:14:17 -0600

    reposive

--- src/compontes/Contact.jsx
+++ src/compontes/Contact.jsx
@@ -1,19 +1,25 @@
 import Footer from './Footer';
 import { useEffect } from 'react';
+
 export default function Contact() {
   useEffect(() => {
     // Desplaza la página al principio cuando se carga
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b text-white">
-      <h2 className="font-serif text-5xl font-bold mb-4 text-center mt-10">Contact Me</h2>
-      <p className="text-lg mb-6 text-center">Feel free to reach out to me via email:</p>
+      <h2 className="font-serif text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center mt-10">
+        Contact Me
+      </h2>
+      <p className="text-base md:text-lg lg:text-xl mb-6 text-center">
+        Feel free to reach out to me via email:
+      </p>
       <a
         href="mailto:Bayronvillanea.info@gmail.com"
-        className="text-blue-300 font-semibold text-xl hover:underline"
+        className="text-blue-300 font-semibold text-base md:text-xl hover:underline"
       >
-        Bayronvillanea.info@gmail.com   
+        Bayronvillanea.info@gmail.com
       </a>
       <footer className="mt-8">
         <Footer />
